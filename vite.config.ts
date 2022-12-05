752ec1ea4696277559ae41bf01a987bb2a8193b8
tanchian <tanchian@shencom.cn>
2022-12-05 11:42:09 +0800

    chore(husky): 添加代码检查和 commit 检查

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteEslint from 'vite-plugin-eslint';
 // vite.config.ts
 import Unocss from 'unocss/vite';
 import { presetAttributify, presetUno } from 'unocss';
+import viteStylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     react(),
     viteEslint(),
+    viteStylelint(),
     Unocss({
       presets: [
         presetAttributify({
