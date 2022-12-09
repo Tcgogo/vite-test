eec9377aac4e4960fe1f846da5cf625db63b491e
tanchian <tanchian@shencom.cn>
2022-12-09 18:22:26 +0800

    feat: 静态资源处理

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,55 @@
-import './index.scss';
+import { ReactComponent as ReactLogo } from '@assets/react.svg';
+import { useEffect } from 'react';
+import styles from './index.module.scss';
+// import Worker from './worker.js?worker';';
+import init from './fib.wasm?init';
+// // 1. 初始化 Worker 实例
+// const worker = new Worker();
+type FibFunc = (num: number) => number;
+// // 2. 主线程监听 worker 的信息
+// worker.addEventListener('message', (e) => {
+//   console.log(e.data);
+// });
+import img1 from '@assets/img1.png';
+import img2 from '@assets/img2.jpg';
+import SvgIcon from '../SvgIcon';
+
+import ids from 'virtual:svg-icons-names';
+
+console.log(
+  '%c [ids]-18',
+  'font-size:13px; background:#336699; color:#fff;',
+  ids
+);
+
+const imgs = Object.values(ids).map((mod) => {
+  const fileName = mod.split('-').pop();
+  const [svgName] = fileName.split('.');
+  return svgName;
+});
+
+init({}).then(({ exports }) => {
+  console.log(
+    '%c [exports]-15',
+    'font-size:13px; background:#336699; color:#fff;',
+    exports.memory
+  );
+  const fibFunc = exports.fib as FibFunc;
+  console.log(
+    '%c [fibFunc]-21',
+    'font-size:13px; background:#336699; color:#fff;',
+    fibFunc
+  );
+  console.log('Fib result:', fibFunc(10));
+});
 
 export function Header() {
-  return <p className="color-fuchsia">This is Header</p>;
+  return (
+    <p className={`text-15px text-center ${styles.header}`}>
+      {imgs.map((img) => (
+        <SvgIcon name={img} key={img} className="w-20 h-20"></SvgIcon>
+      ))}
+      <ReactLogo></ReactLogo>
+    </p>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,41 @@ import viteEslint from 'vite-plugin-eslint';
 import Unocss from 'unocss/vite';
 import { presetAttributify, presetUno } from 'unocss';
 import viteStylelint from 'vite-plugin-stylelint';
+import svgr from 'vite-plugin-svgr';
+import viteImagemin from 'vite-plugin-imagemin';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
+const IsPro = process.env.NODE_ENV === 'production';
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: path.join(__dirname, "src"),
+  // root: './',
+  build: {
+    sourcemap: IsPro,
+    // 如果静态资源体积 >= 8KB，则提取成单独的文件
+    // 如果静态资源体积 < 8KB，则作为 base64 格式的字符串内联
+    assetsInlineLimit: 8 * 1024
+  },
+  // 预构建配置
+  optimizeDeps: {
+    // force: true,
+    // 指定入口文件
+    // entries: []
+    // 决定了可以强制预构建的依赖项
+    // include: ["lodash-es", "vue",
+    // 间接依赖的声明语法，通过`>`分开, 如`a > b`表示 a 中依赖的 b
+    // "@loadable/component > hoist-non-react-statics",]
+    // exclude: []
+  },
+  resolve: {
+    alias: {
+      '@assets': path.join(__dirname, './src/assets')
+    }
+  },
   plugins: [
     react(),
     viteEslint(),
     viteStylelint(),
+    svgr(),
     Unocss({
       presets: [
         presetAttributify({
@@ -22,6 +49,31 @@ export default defineConfig({
         presetUno()
         // ...custom presets
       ]
+    }),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7
+      },
+      // 有损压缩配置，有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9]
+      },
+      // svg 优化
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox'
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false
+          }
+        ]
+      }
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.join(__dirname, 'src/assets/')]
     })
   ]
 });
